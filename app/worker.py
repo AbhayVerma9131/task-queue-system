acc52f843dd117e9d28b899a5972a0a4cb164470
Abhay Verma <99489290+AbhayVerma9131@users.noreply.github.com>
2026-03-31 02:50:49 +0530

    Update worker.py

--- app/worker.py
+++ app/worker.py
@@ -1,16 +1,18 @@
-import redis
-import json
-from tasks import process_task
-
-r = redis.Redis(host="localhost", port=6379, decode_responses=True)
-
-print("Worker started...")
-
 while True:
     task = r.brpop("task_queue")
+
     if task:
         _, task_data = task
         task_json = json.loads(task_data)
 
+        task_json["status"] = "processing"
+        r.set(task_json["id"], json.dumps(task_json))
+
         result = process_task(task_json["data"])
-        print("Processed:", result).
+
+        task_json["status"] = "completed"
+        task_json["result"] = result
+
+        r.set(task_json["id"], json.dumps(task_json))
+
+        print("Done:", result)
